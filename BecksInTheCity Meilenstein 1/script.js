2291b750d8306368dbf3efe7dedfa5423ed83fb0
Deryuu <TuananhVu@hotmail.de>
2017-05-08 13:56:45 +0200

    Wer ist der beste

--- BecksInTheCity Meilenstein 1/script.js
+++ BecksInTheCity Meilenstein 1/script.js
@@ -1,7 +1,12 @@
-function validateForm() {
-  var x = document.forms["myForm"]["FirstName"].value;
-    if (x == "") {
-        alert("Name must be filled out");
+function validateForm(form) {
+    if (form.FirstName.value == "" ) {
+        alert(unescape("Bitte kein leeres Feld! @Vorname"));
+        document.myForm.name.focus();
+        return false;
+    }
+    if (!/^[a-zA-Z]*$/g.test(form.FirstName.value)) {
+        alert("Nur Buchstaben! @Vorname");
+        document.myForm.name.focus();
         return false;
     }
 }
