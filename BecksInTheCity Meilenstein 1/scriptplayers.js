740fb1a2c2514779be9c341ecb626fe32a827e40
Deryuu <TuananhVu@hotmail.de>
2017-05-15 08:20:38 +0200

    Alpha Version

--- BecksInTheCity Meilenstein 1/scriptplayers.js
+++ BecksInTheCity Meilenstein 1/scriptplayers.js
@@ -1,13 +1,15 @@
-document.myForm.addEventListener("load", empfangen());
+var old = document.getElementById("playerTableStyle");
+document.myForm.addEventListener("load", empfangen(old));
 
-function empfangen(){
+
+function empfangen(old){
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if(xhr.readyState === XMLHttpRequest.DONE && xhr.status==200){
       var serverResponse = JSON.parse(xhr.responseText);
       for(var k in serverResponse) {
           console.log(serverResponse[k]);
-          document.getElementById("playerTableStyle").innerHTML +=
+          old.innerHTML +=
           "<tr> <td>" +serverResponse[k].name +
           "</td> <td>"+serverResponse[k].club +
           "</td> <td>"+serverResponse[k].coach +
@@ -21,4 +23,38 @@ function empfangen(){
   };
   xhr.open('GET', 'http://188.166.165.74:13337/api/players', true);
   xhr.send();
+}
+
+function toggle(old){
+  if(document.getElementById("All").innerHTML == "All"){
+  document.getElementById("All").innerHTML  = "";
+  document.getElementById("Favorites").innerHTML  = "Favorites";
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status==200){
+      var serverResponse = JSON.parse(xhr.responseText);
+      for(var k in serverResponse) {
+          console.log(serverResponse[k]);
+          old.innerHTML +=
+          "<tr> <td>" +serverResponse[k].name +
+          "</td> <td>"+serverResponse[k].club +
+          "</td> <td>"+serverResponse[k].coach +
+          "</td> <td>"+serverResponse[k].position +
+          "</td> <td>"+serverResponse[k].number +
+          "</td> <td>"+serverResponse[k].year +"</td> </tr>";
+        }
+    }else if(xhr.readyState === XMLHttpRequest.DONE){
+      console.log(xhr.status);
+      }
+  };
+  xhr.open('GET', 'http://188.166.165.74:13337/api/players?favorites=true', true);
+  xhr.send();
+
+  }else{
+    document.getElementById("Favorites").innerHTML  = "";
+    document.getElementById("All").innerHTML  = "All";
+    empfangen();
+  }
+
 }
